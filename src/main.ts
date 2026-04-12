54b78508297942490cda559b84677b6e1916f1b5
Kyomik <almrilhamm@gmail.com>
2026-04-12 20:37:25 +0800

    penambahan pertahanan endpoint .env di main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,18 +8,26 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get(ConfigService);
-
   const port = configService.get<number>('PORT') || 3000;
 
+  // 🔥 PROTEKSI
+  app.use((req, res, next) => {
+    if (req.url.startsWith('/.') || req.url.includes('.env')) {
+      return res.status(403).send('Forbidden');
+    }
+    next();
+  });
+
   app.useWebSocketAdapter(new WsAdapter(app));
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
       transform: true
     })
-  )
-  
+  );
+
   await app.listen(port);
 
   console.log(`Application is running on: ${await app.getUrl()}`);
